e79194f8a12cc2a08827b0456c3f00201ef2d9ed
Krzysztof Pikóra <krzysztof.pikora@gmail.com>
2017-08-05 14:38:54 +0200

    Checking if the PlayerContoller is accesable form VS and if it's possesing Tank

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -3,8 +3,22 @@
 #include "TankPlayerController.h"
 
 
+void ATankPlayerController::BeginPlay() {
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController it's accesable from VS."))
 
+	ATank* ControlledTank = nullptr;
+	ControlledTank = GetControlledTank();
+	if (!ControlledTank) {
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possesing a tank."), *ControlledTank->GetName())
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController is possesing %s tank."), *ControlledTank->GetName())
+	}
+}
 
 ATank*  ATankPlayerController::GetControlledTank() const {
 	return Cast<ATank>(GetPawn());
 }
+
+
