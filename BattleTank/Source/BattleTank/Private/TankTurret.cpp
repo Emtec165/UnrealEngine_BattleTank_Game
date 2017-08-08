f64ab019f72c04bf984597088b08efd49d06dbfb
Krzysztof Pikóra <krzysztof.pikora@gmail.com>
2017-08-08 21:16:46 +0200

    Added TankTurret cpp class based on StaticMeshComponent. Replacet old Turret based on StaticMeshComponent with new TankTurret.

---
+++ BattleTank/Source/BattleTank/Private/TankTurret.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankTurret.h"
+
+void UTankTurret::Rotate(float RelativeSpeed) {
+	//Rotate turret
+	UE_LOG(LogTemp, Warning, TEXT("Rotating turret"))
+}
+
+
