79ff799b69d920dbf531d608eb1daff118884d86
Krzysztof Pikóra <krzysztof.pikora@gmail.com>
2017-08-10 17:18:05 +0200

    Added TankTrack cpp class and replaced static mesh based components form Tank_BP with it. Mapped gamepad left and right trigger. Added triggers value input methods.

--- BattleTank/Source/BattleTank/Private/Projectile.cpp
+++ BattleTank/Source/BattleTank/Private/Projectile.cpp
@@ -30,7 +30,6 @@ void AProjectile::Tick(float DeltaTime)
 }
 
 void AProjectile::LaunchProjectile(float Speed) {
-	UE_LOG(LogTemp, Warning, TEXT("Fire!"))
 	ProjectileMovement->SetVelocityInLocalSpace(FVector::ForwardVector * Speed);
 	ProjectileMovement->Activate();
 }
---
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankTrack.h"
+
+
+void UTankTrack::SetThrottle(float Throttle) {
+	auto Name = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s throttle: %f"), *Name, Throttle)
+
+	// TODO clamp actual throttle value so player can't change axix scaling and move faster
+}
+
