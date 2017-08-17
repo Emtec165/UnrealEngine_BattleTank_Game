307396a8ad3bb74232e526ff36c2acfe59462aff
Krzysztof Pikóra <krzysztof.pikora@gmail.com>
2017-08-17 22:53:40 +0200

    Getting OnHit() event in track cpp when track colides with something (e.g. grass).

--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -25,5 +25,10 @@ public:
 private:
 	UTankTrack();
 
+	virtual void BeginPlay() override;
+
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction * ThisTickFunction) override;
+
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
 };
